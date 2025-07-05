4c33c3f5ba0808e1202083429eb2874b5ce06ea3
WilleIshere <wille2008080808@gmail.com>
2025-07-05 22:29:14 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,41 +2,28 @@ from src.utils.ffmpeg import check_ffmpeg
 from src.text_to_speech.tts_wrapper import *
 from src.utils.logger import info
 
+import torch
+from TTS.api import TTS
+
 def main():
     info("Starting main process")
     ffmpeg_path = check_ffmpeg()
     info(f"ffmpeg path: {ffmpeg_path}")
 
-    import torch
-    from TTS.api import TTS
-
-    # Get device
+    info("Checking device...")
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    info("Using device 'CPU', cuda not available..." if device == 'cpu' else "Using device 'GPU', cuda installation found...")
 
-    # List available 🐸TTS models
-    print(TTS().list_models())
+    info("Loading tts engine...")
+    tts = TTS("tts_models/en/ljspeech/tacotron2-DDC").to(device)
 
-    # Initialize TTS
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+    speakers = tts.speakers
+    for i, voice in enumerate(speakers):
 
-    # List speakers
-    print(tts.speakers)
-
-    # Run TTS
-    # ❗ XTTS supports both, but many models allow only one of the `speaker` and
-    # `speaker_wav` arguments
-
-    # TTS with list of amplitude values as output, clone the voice from `speaker_wav`
-    wav = tts.tts(
-      text="Hello world!",
-      speaker_wav="my/cloning/audio.wav",
-      language="en"
-    )
-
-    # TTS to a file, use a preset speaker
-    tts.tts_to_file(
-      text="Hello world!",
-      speaker="Craig Gutsy",
-      language="en",
-      file_path="output.wav"
-    )
+        info(f"Generating audio.. ({i + 1}/{len(speakers)})")
+        tts.tts_to_file(
+            text="This is a test of the text-to-speech system. The quick brown fox jumps over the lazy dog. We are generating this audio to ensure that the TTS engine is working correctly and can handle longer sentences with natural prosody and clarity. Thank you for listening to this demonstration.",
+            speaker="Uta Obando",
+            language="en",
+            file_path=f"tts_output/{voice}.wav"
+        )
