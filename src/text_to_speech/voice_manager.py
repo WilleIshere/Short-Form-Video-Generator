1ec4125e8fa1e53e0ba5619954b7160fec926aa2
WilleIshere <wille2008080808@gmail.com>
2025-07-06 05:28:36 +0200

    Update voice_manager.py

--- src/text_to_speech/voice_manager.py
+++ src/text_to_speech/voice_manager.py
@@ -1,18 +1,55 @@
+
 import os
+
 import json
+
 from pprint import pprint
+
 import requests
-import shutil
+
 import typing
+import zipfile
+import shutil
+
+
 
 VOICES = os.path.join(os.getcwd(), 'voices')
+
 VOICES_JSON = os.path.join(os.getcwd(), 'voices.json')
+VOICES_EN_US_ZIP = os.path.join(VOICES, 'en_US.zip')
+VOICES_EN_GB_ZIP = os.path.join(VOICES, 'en_GB.zip')
 
 def update_voices():
+    # --- ZIP extraction logic ---
+    for file in os.listdir(VOICES):
+        if file.endswith('.zip'):
+            zip_path = os.path.join(VOICES, file)
+            extract_folder = os.path.join(VOICES, file[:-4])  # Remove .zip extension
+
+            zip_mtime = os.path.getmtime(zip_path)
+            folder_exists = os.path.isdir(extract_folder)
+
+            needs_extract = False
+            if not folder_exists:
+                needs_extract = True
+            else:
+                folder_mtime = os.path.getmtime(extract_folder)
+                if zip_mtime > folder_mtime:
+                    shutil.rmtree(extract_folder)
+                    needs_extract = True
+
+            if needs_extract:
+                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(extract_folder)
+                os.utime(extract_folder, (zip_mtime, zip_mtime))
+    # --- End ZIP extraction logic ---
+
     voices = {}
 
     for lang in os.listdir(VOICES):
         voice_folder = os.path.join(VOICES, lang)
+        if not os.path.isdir(voice_folder):
+            continue
         voices[lang] = {}
 
         for voice in os.listdir(voice_folder):
@@ -31,13 +68,8 @@ def update_voices():
                     if file.endswith('.onnx.json'):
                         voices[lang][voice][quality]['config_path'] = file_path
 
-                    if file == "samples":
-                        shutil.rmtree(file_path)
-
-
     with open(VOICES_JSON, 'w') as f:
         json.dump(voices, f)
-
 
 def list_voices() -> dict:
     with open(VOICES_JSON, 'r') as f:
