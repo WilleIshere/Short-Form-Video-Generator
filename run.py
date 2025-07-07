6f0b48e87f73e4c29e75c53cf4da6998210ff1ff
WilleIshere <wille2008080808@gmail.com>
2025-07-07 14:08:11 +0200

    Fix ffmpeg path for whisper

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ if __name__ == '__main__':
     print('Starting up...')
 
     print('Setting environment variables...')
-    import os
 
-os.environ["PATH"] = "/your/custom/ffmpeg/folder:" + os.environ["PATH"]
+    import os
+    ffmpeg_path = "ffmpeg_bin/ffmpeg"
+    ffmpeg_dir = os.path.abspath(os.path.dirname(ffmpeg_path))
+    os.environ['PATH'] = f'{ffmpeg_dir}:{os.environ["PATH"]}'
 
     print('Running application...')
     main()
