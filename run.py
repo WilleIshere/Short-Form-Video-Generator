eead4ad23b5c7c4693397404b9cd0e93881f8f78
WilleIshere <wille2008080808@gmail.com>
2025-07-07 14:07:52 +0200

    Create __init__.py

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from src.main import main
 
 if __name__ == '__main__':
+    print('Starting up...')
+
+    print('Setting environment variables...')
+    import os
+
+os.environ["PATH"] = "/your/custom/ffmpeg/folder:" + os.environ["PATH"]
+
+    print('Running application...')
     main()
