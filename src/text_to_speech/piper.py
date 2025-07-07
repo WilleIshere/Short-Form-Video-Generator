3c937d251fcce423b408cc1556e83c1805e8e4be
WilleIshere <wille2008080808@gmail.com>
2025-07-07 11:55:25 +0200

    Update piper.py

--- src/text_to_speech/piper.py
+++ src/text_to_speech/piper.py
@@ -1,27 +1,17 @@
 import os
+from pprint import pprint
 import subprocess
 
 PIPER = os.path.join(os.getcwd(), 'piper', 'piper.exe')
+OUTPUT_DIR = os.path.join(os.getcwd(), 'tts_output')
 
 def generate(text: str, voice_path: str, config_path: str, output_path: str):
-    print('Generating tts...')
-    print(f"[DEBUG] Text to synthesize:\n{text}\n")
     cmd = [
         PIPER,
         "-m", voice_path,
         "-c", config_path,
-        "-f", os.path.join(os.getcwd(), output_path),
+        "-f", os.path.join(OUTPUT_DIR, output_path),
         "--debug",
-        "--input-stdin"
     ]
-    print(f"[DEBUG] Command to run:\n{' '.join(cmd)}\n")
     proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate(input=text.encode("utf-8"))
-    print('done!')
-    print("[DEBUG] Piper stdout:\n", stdout.decode(errors="replace"))
-    print("[DEBUG] Piper stderr:\n", stderr.decode(errors="replace"))
-    output_full_path = os.path.join(os.getcwd(), output_path)
-    if os.path.exists(output_full_path):
-        print(f"[DEBUG] Output file generated: {output_full_path}")
-    else:
-        print(f"[DEBUG] Output file NOT found: {output_full_path}")
