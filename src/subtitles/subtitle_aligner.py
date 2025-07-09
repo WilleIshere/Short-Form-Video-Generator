e48219215e59cb37b6fe83537d216dc24b372499
WilleIshere <wille2008080808@gmail.com>
2025-07-09 14:55:01 +0200

    Update subtitle_aligner.py

--- src/subtitles/subtitle_aligner.py
+++ src/subtitles/subtitle_aligner.py
@@ -1,13 +1,17 @@
 import subprocess
+from rich.console import Console
 
 AENEAS_BIN = 'aeneas-cli.exe'
 
 def align_audio_to_text(audio_path: str, text_path: str, out_path: str):
-    cmd = [
-        AENEAS_BIN,
-        audio_path,                         # input audio file
-        text_path,                              # input text file
-        'task_language=eng|is_text_type=plain|os_task_file_format=json|os_task_file_levels=1',  # config string
-        out_path                               # output file
-    ]
-    subprocess.run(cmd, check=True)
+    console = Console()
+    with console.status('Aligning tts to text...'):
+        cmd = [
+            AENEAS_BIN,
+            audio_path,                         # input audio file
+            text_path,                              # input text file
+            'task_language=eng|is_text_type=plain|os_task_file_format=json|os_task_file_levels=1',  # config string
+            out_path,
+            "--verbose"        # output file
+        ]
+        subprocess.run(cmd, check=True)
